aa640743aa52bc6bcca2c236a1f8ecdea919ebea
duht <dl_dht@aliyun.com>
2015-09-09 22:54:33 +0800

    dev

--- tianya.sh
+++ tianya.sh
@@ -2,21 +2,25 @@
 
 if [ $# != 3 ];then
 	echo ./tianya.sh save_filename url end_index.
-	echo ./tianya.sh 帝国最后的荣耀大明1592抗日援朝.txt http://bbs.tianya.cn/post-no05-184314 119
+	echo ./tianya.sh 帝国最后的荣耀大明1592抗日援朝 http://bbs.tianya.cn/post-no05-184314 119
 	exit;
 fi
 
 end_index=$3
 url=$2
-saved_filename=$1;
+saved_tmp_filename=$1;
+saved_filename=${1}.txt;
 
 #echo $saved_filename : $url : $end_index
 
 for i in `seq 1 $end_index`
 do
-	lynx -dump $url-$i.shtml | ./filter.pl >> $saved_filename
+	lynx -dump $url-$i.shtml | ./filter.pl >> $saved_tmp_filename
 done
 
+./merge_line.pl $saved_tmp_filename > $saved_filename
+#cp $saved_tmp_filename .a.txt
+rm -f $saved_tmp_filename
 #============================ filter.pl  ===============================
 #!/usr/bin/perl -w
 
